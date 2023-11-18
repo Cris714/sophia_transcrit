cae0ae2915933ea91ea83ab7a923c8ac22eab3dc
isiIH <isidora.iyho@gmail.com>
2023-11-18 18:21:30 -0300

    login con google agregado

---
+++ lib/app_provider.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sophia_transcrit2/get_audio_page.dart';
+import 'file_manager_s.dart';
+
+Map<int, String> statusError = {
+  0: 'Connexion Timeout. Connect to internet',
+  504: 'Server not responding. Try again later.',
+  404: 'File not found.',
+  500: 'Internal server error.',
+  502: 'Bad Gateway.'
+};
+
+class ListItem {
+  String text;
+  bool checked;
+
+  ListItem(this.text, this.checked);
+}
+
+class errorItem {
+  String text;
+  int statusCode;
+
+  errorItem(this.text, this.statusCode);
+}
+
+class AppProvider extends ChangeNotifier { // create a common file for data
+  Widget _currentScreen = const GetAudioPage();
+  int _currentTab = 1;
+  List<ListItem> _fileTrans = [];
+  String _folderTrans = "";
+  bool _showCardTrans = false;
+  bool _showErrors = false;
+  List<ListItem> _fileDocs = [];
+  String _folderDocs = "";
+  bool _showCardDocs = false;
+  List<errorItem> _errors = [];
+  bool _showDocsErrors = false;
+  List<errorItem> _docsErrors = [];
+  bool _showCardAudio = false;
+  GoogleSignInAccount? _user;
+
+  Widget get currentScreen => _currentScreen;
+  int get currentTab => _currentTab;
+  List<ListItem> get fileTrans => _fileTrans;
+  String get folderTrans => _folderTrans;
+  List<ListItem> get fileDocs => _fileDocs;
+  String get folderDocs => _folderDocs;
+  bool get showCardTrans => _showCardTrans;
+  bool get showErrors => _showErrors;
+  bool get showCardDocs => _showCardDocs;
+  bool get showDocsErrors => _showDocsErrors;
+  List<errorItem> get errors => _errors;
+  List<errorItem> get docsErrors => _docsErrors;
+  bool get showCardAudio => _showCardAudio;
+  GoogleSignInAccount? get user => _user;
+
+  void setUser(GoogleSignInAccount newUser) {
+    _user = newUser;
+    notifyListeners();
+  }
+
+  void clearErrors(){
+    _errors.clear();
+    notifyListeners();
+  }
+
+  void clearDocsErrors(){
+    _docsErrors.clear();
+    notifyListeners();
+  }
+
+  void addDocsError(errorItem newError){
+    _docsErrors.add(newError);
+    notifyListeners();
+  }
+
+  void addError(errorItem newError){
+    _errors.add(newError);
+    notifyListeners();
+  }
+
+  void setShowDocsErrors(newBool) {
+    _showDocsErrors = newBool;
+    notifyListeners();
+  }
+
+  void setShowErrors(newBool) {
+    _showErrors = newBool;
+    notifyListeners();
+  }
+
+  void setShowCardTrans(newBool) {
+    _showCardTrans = newBool;
+    notifyListeners();
+  }
+
+  void setShowCardDocs(newBool) {
+    _showCardDocs = newBool;
+    notifyListeners();
+  }
+
+  void setShowCardAudio(newBool) {
+    _showCardAudio = newBool;
+    notifyListeners();
+  }
+
+  void setScreen(Widget newScreen, int newTab) {
+    _currentScreen = newScreen;
+    _currentTab = newTab;
+
+    if(newTab != 0) {
+      for (var f in _fileTrans) {
+        f.checked = false;
+      }
+    } else if(newTab != 2) {
+      for (var f in _fileDocs) {
+        f.checked = false;
+      }
+    }
+
+    notifyListeners();
+  }
+
+  void setTranscriptions(newFileObj) {
+    _fileTrans = newFileObj;
+    notifyListeners();
+  }
+
+  void setDocuments(newFileObj) {
+    _fileDocs = newFileObj;
+    notifyListeners();
+  }
+
+  Future<void> getTranscriptions() async {
+    _folderTrans = await createFolderInAppDocDir("transcriptions");
+    final filenames = await getFilesInFolder(_folderTrans);
+    final files = filenames.map((text) => ListItem(text, false)).toList();
+    setTranscriptions(files);
+    notifyListeners();
+  }
+
+  Future<void> getDocuments() async {
+    _folderDocs = await createFolderInAppDocDir("documents");
+    final filenames = await getFilesInFolder(_folderDocs);
+    final files = filenames.map((text) => ListItem(text, false)).toList();
+    setDocuments(files);
+    notifyListeners();
+  }
+
+  AppProvider() {
+    getTranscriptions();
+    getDocuments();
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,344 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+import 'app_provider.dart';
+import 'home.dart';
+
+class GoogleSignInApi {
+  static final _googleSignIn = GoogleSignIn();
+
+  static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
+
+  static Future logout() => _googleSignIn.disconnect();
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _rememberMe = false;
+  late AppProvider appProvider;
+
+  Future signIn() async {
+    final user = await GoogleSignInApi.login();
+    if(user == null){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sign In Failed'),backgroundColor: Colors.red,));
+    } else {
+      appProvider.setUser(user);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    }
+  }
+
+  Widget _buildEmailTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Email',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPasswordTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Password',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildForgotPasswordBtn() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        onPressed: () => print('Forgot Password Button Pressed'),
+        child: Text(
+          'Forgot Password?',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRememberMeCheckbox() {
+    return Container(
+      height: 20.0,
+      child: Row(
+        children: <Widget>[
+          Theme(
+            data: ThemeData(unselectedWidgetColor: Colors.white),
+            child: Checkbox(
+              value: _rememberMe,
+              checkColor: Colors.green,
+              activeColor: Colors.white,
+              onChanged: (value) {
+                setState(() {
+                  _rememberMe = value!;
+                });
+              },
+            ),
+          ),
+          Text(
+            'Remember me',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () => print('Login Button Pressed'),
+        child: Text(
+          'LOGIN',
+          style: TextStyle(
+            color: Color(0xFF527DAA),
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInWithText() {
+    return Column(
+      children: <Widget>[
+        Text(
+          '- OR -',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          'Sign in with',
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSocialBtn(Function onTap, AssetImage logo) {
+    return GestureDetector(
+      onTap: onTap(),
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          image: DecorationImage(
+            image: logo,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialBtnRow() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          // _buildSocialBtn(
+          //       () => print('Login with Facebook'),
+          //   AssetImage(
+          //     'assets/logos/facebook.jpg',
+          //   ),
+          // ),
+          // _buildSocialBtn(
+          //       _handleSignIn,
+          //   AssetImage(
+          //     'assets/logos/google.jpg',
+          //   ),
+          // ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 25.0),
+            width: 200,
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              icon: const FaIcon(
+                FontAwesomeIcons.google,
+                color: Colors.red,
+              ),
+              onPressed: signIn,
+              label: const Text('Sign Up with Google'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignupBtn() {
+    return GestureDetector(
+      onTap: () => print('Sign Up Button Pressed'),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Don\'t have an Account? ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            TextSpan(
+              text: 'Sign Up',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    appProvider = Provider.of<AppProvider>(context, listen: true);
+
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF73AEF5),
+                      Color(0xFF61A4F1),
+                      Color(0xFF478DE0),
+                      Color(0xFF398AE5),
+                    ],
+                    stops: [0.1, 0.4, 0.7, 0.9],
+                  ),
+                ),
+              ),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 120.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'OpenSans',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      _buildEmailTF(),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      _buildPasswordTF(),
+                      _buildForgotPasswordBtn(),
+                      _buildRememberMeCheckbox(),
+                      _buildLoginBtn(),
+                      _buildSignInWithText(),
+                      _buildSocialBtnRow(),
+                      _buildSignupBtn(),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
